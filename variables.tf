ec38ee20d0bda12691ee3ec809529b725f0d9705
DaniilInside <58481217+DaniilInside@users.noreply.github.com>
2025-05-23 16:08:45 +0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "folder_id" {
+  description = "The folder ID for the instance"
+  default     = "" # b1gbn...
+}
+variable "network_id" {
+  description = "The network ID for the instance"
+  default     = "" # enph1...
+}
+variable "zone" {
+  description = "YC zone"
+  default     = "" # ru-central1-...
+}
